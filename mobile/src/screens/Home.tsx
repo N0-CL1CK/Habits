39cc9e9638bd236bc445f9f7d180796eb4fcb628
N0-CL1CK <luis.gsn@outlook.com>
2023-01-21 11:40:22 -0400

    criado o visual e logica principal da homepage com os frames de cada dia

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,51 @@
-import { View } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import { DayHabit, FRAME_SIZE } from "../components/DayHabit";
 import { Header } from "../components/Header";
+import { generateDates } from "../utils/generate-dates";
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+const summaryDates = generateDates();
+
+const summaryDatesSize = (7 * 13);
+const daysToFill = (summaryDatesSize - summaryDates.length);
 
 export function Home() {
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
+
+            <View className="flex-row mt-6 mb-2">
+                {weekDays.map((weekDay, i) => (
+                    <Text
+                        key={`${weekDay}-${i}`}
+                        className="text-zinc-400 text-xl font-bold text-center m-1"
+                        style={{ width: FRAME_SIZE }}
+                    >
+                        {weekDay}
+                    </Text>
+                ))}
+            </View>
+
+            <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 100 }}>
+                <View className="flex-row flex-wrap">
+                    {summaryDates.map(date => (
+                        <DayHabit
+                            key={date.toISOString()}
+                        />
+                    ))}
+
+                    {daysToFill > 0 && Array
+                        .from({ length: daysToFill })
+                        .map((_, i) => (
+                            <View
+                                key={i}
+                                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                                style={{ width: FRAME_SIZE, height: FRAME_SIZE }}
+                            />
+                        ))
+                    }
+                </View>
+            </ScrollView>
         </View>
     )
 }
