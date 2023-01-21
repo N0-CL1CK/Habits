26a481473c35603bc608915cf44f7fd882d93a03
N0-CL1CK <luis.gsn@outlook.com>
2023-01-21 01:32:51 -0400

    configurado o uso de algumas bibliotecas como nativewind, svg transformer e svg; criado o header da homepage

---
+++ mobile/src/components/Header.tsx
@@ -0,0 +1,25 @@
+import { TouchableOpacity, View, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+import LogoImage from '../assets/logo.svg';
+import colors from 'tailwindcss/colors';
+
+export function Header() {
+    return (
+        <View className='w-full flex-row items-center justify-between'>
+            <LogoImage />
+            <TouchableOpacity activeOpacity={0.3} 
+                className='flex-row h-11 px-4 border border-violet-500 rounded-lg items-center'
+            >
+                <Feather
+                    name='plus'
+                    color={colors.violet[500]}
+                    size={20}
+                />
+                <Text className='text-white ml-3 font-semibold text-base'>
+                    Novo
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
