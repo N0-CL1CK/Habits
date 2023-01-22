53a3a437dfc642e68f7554479f03d95395a2c693
N0-CL1CK <luis.gsn@outlook.com>
2023-01-22 15:29:54 -0400

    integrado uma parte do backend ao frontend, partes nas quais incluem os detalhes dos dias (como dia da semana e data), criação de novos habitos, restando somente integrar a ProgressBar e os checklist dos habitos diarios

--- web/src/components/DayHabits.tsx
+++ web/src/components/DayHabits.tsx
@@ -1,18 +1,24 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
 import * as Popover from '@radix-ui/react-popover';
 import { ProgressBar } from './ProgressBar';
 import clsx from 'clsx';
+import { Check } from 'phosphor-react';
+import dayjs from 'dayjs';
 
 interface dayHabitsProps {
-    completed: number
-    amount: number
+    date: Date
+    amount?: number
+    completed?: number
 }
 
-export function DayHabits({ completed, amount }: dayHabitsProps) {
-    const completedPercentual = Math.round((completed/amount)*100);
+export function DayHabits({ completed = 0, amount = 0, date }: dayHabitsProps) {
+    const completedPercentual = amount > 0 ? Math.round((completed/amount)*100) : 0;
+    const dayAndMonth = dayjs(date).format('DD/MM');
+    const weekDay = dayjs(date).format('dddd');
 
     return (
         <Popover.Root>
-            <Popover.Trigger className={clsx("text-zinc-900 w-10 h-10 border-2 border-zinc-800 rounded-lg", {
+            <Popover.Trigger className={clsx('text-zinc-900 w-10 h-10 border-2 border-zinc-800 rounded-lg', {
                 'bg-zinc-900 border-zinc-800': completedPercentual === 0,
                 'bg-violet-900 border-violet-700': completedPercentual > 0 && completedPercentual < 20,
                 'bg-violet-800 border-violet-600': completedPercentual >= 20 && completedPercentual < 40,
@@ -21,11 +27,26 @@ export function DayHabits({ completed, amount }: dayHabitsProps) {
                 'bg-violet-500 border-violet-300': completedPercentual >= 80
             })} />
             <Popover.Portal>
-                <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-                    <span className="font-semibold text-zinc-400">Domingo</span>
-                    <span className="mt-1 font-extrabold leading-tight text-3xl">22/01</span>
+                <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
+                    <span className='font-semibold text-zinc-400'>{weekDay}</span>
+                    <span className='mt-1 font-extrabold leading-tight text-3xl'>{dayAndMonth}</span>
                     <ProgressBar progress={50} />
-                    <Popover.Arrow height={10} width={20} className="fill-zinc-900" />
+                    <div className='mt-6 flex flex-col gap-3'>
+                        <Checkbox.Root className='flex items-center gap-3 group'>
+                            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                                <Checkbox.Indicator>
+                                    <Check
+                                        size={20}
+                                        className='text-white'
+                                    />
+                                </Checkbox.Indicator>
+                            </div>
+                            <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-white/70'>
+                                Beber 2 litros de água
+                            </span>
+                        </Checkbox.Root>
+                    </div>
+                    <Popover.Arrow height={10} width={20} className='fill-zinc-900' />
                 </Popover.Content>
             </Popover.Portal>
         </Popover.Root>
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,6 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
 import { generateDates } from "../utils/generate-dates";
 import { DayHabits } from "./DayHabits";
 
@@ -6,7 +9,22 @@ const summaryDates = generateDates();
 const summaryDatesSize = (18 * 7);
 const daysToFill = (summaryDatesSize - summaryDates.length);
 
+type Summary = Array<{
+    id: string,
+    date: string,
+    amount: number,
+    completed: number
+}>
+
 export function SummaryTable() {
+    const [summary, setSummary] = useState<Summary>([]);
+  
+    useEffect(() => {
+        api.get('/summary')
+            .then(response => setSummary(response.data))
+            .catch(reject => console.error(reject));
+    }, []);
+
     return (
         <div className="w-full flex">
             <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -20,11 +38,16 @@ export function SummaryTable() {
             </div>
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map(summaryDate => {
+                    const dayInSummary = summary.find(day => {
+                        return dayjs(summaryDate).isSame(day.date, 'day');
+                    });
+
                     return (
                         <DayHabits
-                            key={summaryDate.toString()}
-                            amount={5}
-                            completed={1}
+                            key={summaryDate.toString()}    
+                            date={summaryDate}
+                            amount={dayInSummary?.amount}
+                            completed={dayInSummary?.completed}
                         />
                     )
                 })}
